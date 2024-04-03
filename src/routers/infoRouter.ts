7a32d6b1052ee2a54404b1f1282e649439a76a4d
nuki1048 <chabaniuk.nik@gmail.com>
2024-04-03 09:56:18 +0300

    feat: all sections

--- src/routers/infoRouter.ts
+++ src/routers/infoRouter.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express';
 import {
   createNewSection,
+  getAllSections,
   getSingleSection,
 } from '../controllers/infoController';
 import { UserRoles, restrictTo } from '../controllers/authController';
 
 const router = Router();
 
-router.route('/').post(restrictTo(UserRoles.Admin), createNewSection);
+router
+  .route('/')
+  .post(restrictTo(UserRoles.Admin), createNewSection)
+  .get(getAllSections);
 router.route('/:slug').get(getSingleSection);
 
 export default router;
