336164efb9fa90c33048a812f0ba1de210c55d42
nuki1048 <chabaniuk.nik@gmail.com>
2024-04-06 14:25:43 +0300

    feat: setting up router

--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -1,8 +1,38 @@
 import { Router } from 'express';
-import { getLogin } from '../controllers/authController';
+
+import { userController, authController } from '../controllers';
+import { UserRoles } from '../models/userModel';
 
 const router = Router();
 
-router.route('/login').get(getLogin);
+router.post('/login', authController.login);
+
+router.post('/signup', authController.signup);
+
+router.post('/forgotPassword', authController.forgotPassword);
+
+router.patch('/resetPassword/:token', authController.resetPassword);
+
+// Protect all routes below that middleware
+router.use(authController.protect);
+
+router.patch('/updatePassword', authController.updatePassword);
+router.get('/me', userController.getMe, userController.getUser);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
+
+// Protect all routes below that middleware
+router.use(authController.restrictTo(UserRoles.Admin));
+
+router
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 export default router;
